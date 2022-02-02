5de439be0e9208c285d26dc8d4b5e2282ff965bb
Jhionan <jhionan@gmail.com>
2022-02-02 01:23:34 -0300

    refactor(fixture): fix name

--- test/feature/home/stubs/search_stub.dart
+++ test/feature/home/stubs/search_stub.dart
@@ -5,6 +5,6 @@ import 'package:market_libre/features/home/data/models/search_dto.dart';
 
 SearchDto searchResultStubed() {
   final Map<String, dynamic> jsonResponse =
-      json.decode(File('test/feature/home/fixure/search_notebook.json').readAsStringSync());
+      json.decode(File('test/feature/home/fixture/search_notebook.json').readAsStringSync());
   return SearchDto.fromJson(jsonResponse);
 }
