70f010a48093ef0685505839a24258c17fc310ca
Jhionan <jhionan@gmail.com>
2022-02-02 01:52:05 -0300

    feat(http): add dio transformer to run decode inside isolate

--- lib/core/http_server/http_client.dart
+++ lib/core/http_server/http_client.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:dio_flutter_transformer2/dio_flutter_transformer2.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class HttpClient {
   HttpClient({required String baseUrl})
       : _dio = Dio()
+          ..transformer = FlutterTransformer()
           ..options = BaseOptions(
             baseUrl: baseUrl,
             receiveTimeout: _timeoutInMillisecond,
@@ -18,8 +20,6 @@ class HttpClient {
               responseHeader: true));
   static const _timeoutInMillisecond = 30000;
   final Dio _dio;
-  
-
 
   Dio get dio => _dio;
 }
